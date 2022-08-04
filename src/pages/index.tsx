2dea0d6c443a494044ecfa95f9530fadc289c9df
Victor Hung <victorhung92@gmail.com>
2022-08-04 03:01:12 -0400

    Add placeholder landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,14 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.app}>
-      hello
+    <div className={styles.centerContainer}>
+      <div className={styles.centerContent}>
+        <h1>Hello!</h1>
+        <p>
+          This is the future website for the 2022 MIT Illuminations First-year Seminar. Check back
+          here for more information soon!
+        </p>
+      </div>
     </div>
   );
 };

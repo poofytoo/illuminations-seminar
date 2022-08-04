cd8c6eb3a1e455b9946aea7f34986b6f24f1de32
Victor Hung <victorhung92@gmail.com>
2022-08-04 02:49:00 -0400

    Initial website scaffold

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.app}>
+      hello
+    </div>
+  );
+};
+
+export default Home;

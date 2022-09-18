5b691e7cdb0474b2cad21b112f08c8241d895506
Victor Hung <victorhung92@gmail.com>
2022-09-18 10:55:23 -0400

    feat: OG and preview image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,29 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-
-import '../styles/globals.scss'
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-    <Head>
-      <title>MIT Illuminations Seminar</title>
-    </Head>
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Head>
+        <title>MIT Illuminations Seminar</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:url" content="https://learn.illuminations.mit.edu" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="MIT Illuminations Seminar" />
+        <meta
+          property="og:description"
+          content="Dynamic Light Installation at the MIT Welcome Center"
+        />
+        <meta
+          property="og:image"
+          content="https://learn.illuminations.mit.edu/images/illuminations-banner.png"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;

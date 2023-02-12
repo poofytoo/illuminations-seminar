97630e8a9b41a75d2a0c757d368e967e97c9854e
RhamzThev <rhamzthev@gmail.com>
2023-02-12 14:01:10 -0500

    feat: added google doc content to slit scan page

--- src/pages/chapter/slit-scan.tsx
+++ src/pages/chapter/slit-scan.tsx
@@ -1,16 +1,45 @@
 import Link from 'next/link';
-import GuideImage from '../../components/GuideImage';
+// import GuideImage from '../../components/GuideImage';
 import Sidebar from '../../components/Sidebar';
 
+import { google } from 'googleapis';
 import styles from '../../styles/Guide.module.scss';
+import GoogleParser from '../../components/GoogleParser';
+
+export async function getStaticProps() {
+  const documentId = '1ZerefWUnmoHVurdRzypmCZ5phsoziux_A_U_1ynTAQo';
+  const client = new google.auth.JWT({
+    email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+    scopes: ['https://www.googleapis.com/auth/documents'],
+    key: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+  });
+
+  await client.authorize();
+
+  const gsapi = google.docs({ version: 'v1', auth: client });
+  const opt = {
+    documentId,
+  };
+
+  const data = await gsapi.documents.get(opt);
+
+  return {
+    props: {
+      data: data.data,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 5 seconds
+    revalidate: 5, // In seconds
+  };
+}
 
 const Page = ({ data }: any) => {
   return (
     <div className={styles.appWrapper}>
       <Sidebar />
       <div className={styles.guideContent}>
-        <h1>Slit Scan</h1>
-        <h2>Coming Soon!</h2>
+        <GoogleParser rawData={data} />
         <p></p>
       </div>
     </div>

b8e721ca2cd63e6a122c78d523557fff7b83aba4
RhamzThev <rhamzthev@gmail.com>
2023-02-14 20:42:44 -0500

    fix: removed google parser - converted to html

--- src/next.config.js
+++ src/next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      'ik.imagekit.io',
-      'storage.googleapis.com',
-      'drive.google.com',
-      'lh1.googleusercontent.com',
-      'lh2.googleusercontent.com',
-      'lh3.googleusercontent.com',
-      'lh4.googleusercontent.com',
-      'lh5.googleusercontent.com',
-      'lh6.googleusercontent.com',
-    ],
-  },
 };
 
 module.exports = nextConfig;
